fe52615281231f2247144b99fc1d10dcac828068
James Edington Administrator <james.edington@uah.edu>
2024-01-11 16:38:42 -0600

    Support ISO-8859-1 on Windows

--- Tesseract/_fix_windows_console.py
+++ Tesseract/_fix_windows_console.py
@@ -9,7 +9,7 @@ def fixit():
     if platform.system() == 'Windows':
         chcp_output = subprocess.check_output(['chcp.com'], encoding='ascii')
         cur_codepage = re.match(r'Active code page: (\d+)', chcp_output).group(1)
-        cur_codepage_name = {437: 'cp437', 65001: 'utf-8'}[int(cur_codepage)]
+        cur_codepage_name = {437: 'cp437', 65001: 'utf-8', 28591: 'iso-8859-1'}[int(cur_codepage)]
         if sys.stdout.encoding != cur_codepage_name:
             sys.stdout.reconfigure(encoding=cur_codepage_name)
         just_fix_windows_console()
